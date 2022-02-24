627df2c17b26e7208d55169e58fe6610dbb64d3e
fedorbondar <bondar.feod@yandex.ru>
2022-02-25 00:48:32 +0300

    add advanced structure

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <vector>
+#include "structure.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    university_game::quest first_quest("first_quest", "1st description", 1);
+    university_game::quest second_quest("second_quest", "2nd description", 2);
+    std::vector<university_game::quest> initial_quests = {first_quest, second_quest};
+    std::vector<university_game::item> initial_items{};
+
+    university_game::player player_1("Fedor", initial_quests, initial_items);
+    university_game::teacher teacher_1("Mr. Antipoff");
+
+    player_1.display_available_quests();
+    player_1.show_inventory();
+
+    university_game::item first_prize("gold medal", "just useless, enjoy");
+    teacher_1.satisfy_quest(player_1, "first_quest", first_prize);
+
+    player_1.display_available_quests();
+    player_1.show_inventory();
     return 0;
 }
